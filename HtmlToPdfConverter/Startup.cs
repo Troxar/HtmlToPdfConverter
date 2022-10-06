78f0399a597ce3896b26fa379905635fd9947ddc
Alexander Katkov <afargoth@gmail.com>
2022-10-06 19:44:50 +0700

    Get file from client

--- HtmlToPdfConverter/Startup.cs
+++ HtmlToPdfConverter/Startup.cs
@@ -24,9 +24,32 @@ public class Startup
         app.UseDefaultFiles();
         app.UseStaticFiles();
         app.UseRouting();
-        app.UseEndpoints(e =>
+        app.UseEndpoints(e => e.MapPost("/api/upload", UploadHandler));
+    }
+
+    async Task<IResult> UploadHandler(HttpRequest request)
+    {
+        var form = await request.ReadFormAsync();
+        if (form.Files.Count == 0)
         {
-            e.MapGet("/api/hello", () => "hello");
-        });
+            return await Task.FromResult(Results.BadRequest("File not received"));
+        }
+        
+        var file = form.Files[0];
+        if (file.Length == 0)
+        {
+            return await Task.FromResult(Results.BadRequest("File is empty"));
+        }
+
+        var originalFileName = Path.GetFileName(file.FileName);
+        var savingFileName = Path.GetRandomFileName();
+        var savingFilePath = Path.Combine(Directory.GetCurrentDirectory(), "upload", savingFileName);
+
+        await using (var stream = File.Create(savingFilePath))
+        {
+            await file.CopyToAsync(stream);
+        }
+        
+        return await Task.FromResult(Results.Ok($"{originalFileName} saved as {savingFileName}"));
     }
 }
